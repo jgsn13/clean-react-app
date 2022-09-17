ec11f519d2fe7dc3ac8c9189ed0df5d18948bf17
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-09-17 18:53:00 -0300

    chore: config jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+};
